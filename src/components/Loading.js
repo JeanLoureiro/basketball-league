186b606cae86e9df69632eaeb0d4812bc473d51c
Jean Loureiro <jean_loureiro@hotmail.com>
2018-03-31 12:27:49 +1000

    [add] Animation on players page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 export default class Loading extends Component {
-  
+
   static defaultProps = {
     text: 'Loading',
   }
